bc981ac9b7e18dfcc688ec2350ba72d8a2ca75e1
jiayi <jiayizhan2005@gmail.com>
2024-06-16 23:12:48 +0200

    ⚡️

--- app/page.jsx
+++ app/page.jsx
@@ -57,7 +57,7 @@ export default function Home() {
     };
   });
 
-  useGSAP(() => {
+  useGSAP((context, contextSafe) => {
     const triggerElement = $(animatedParagraph.current);
     const targetElement = $(".span-line-inner", triggerElement);
 
